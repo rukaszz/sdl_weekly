7fdad94593a30e499ad5b6dc8ff4bc0602307d7d
rukaszz <amusantl@yahoo.co.jp>
2025-11-18 21:32:32 +0900

    make window and renderer class

---
+++ CMakeFiles/sdl_week1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucas/github/sdl_week1/src/Renderer.cpp" "CMakeFiles/sdl_week1.dir/src/Renderer.cpp.o" "gcc" "CMakeFiles/sdl_week1.dir/src/Renderer.cpp.o.d"
+  "/home/lucas/github/sdl_week1/src/Window.cpp" "CMakeFiles/sdl_week1.dir/src/Window.cpp.o" "gcc" "CMakeFiles/sdl_week1.dir/src/Window.cpp.o.d"
+  "/home/lucas/github/sdl_week1/src/main.cpp" "CMakeFiles/sdl_week1.dir/src/main.cpp.o" "gcc" "CMakeFiles/sdl_week1.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/sdl_week1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl_week1.dir/src/Renderer.cpp.o"
+  "CMakeFiles/sdl_week1.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/sdl_week1.dir/src/Window.cpp.o"
+  "CMakeFiles/sdl_week1.dir/src/Window.cpp.o.d"
+  "CMakeFiles/sdl_week1.dir/src/main.cpp.o"
+  "CMakeFiles/sdl_week1.dir/src/main.cpp.o.d"
+  "build/sdl_week1"
+  "build/sdl_week1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl_week1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
